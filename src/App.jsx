9454b91d23da616c55b43fbdf71adeecc56b6ea2
JONAHBRUZZI <jon.guerra@duocuc.cl>
2025-10-05 23:11:37 -0300

    cambios en h1

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1>Hello a todos , esto es una prueba </h1>
+    <h1>Hello a todos darletto, esto es una prueba </h1>
   )
 }
 
